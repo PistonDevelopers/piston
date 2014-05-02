dd3296ecea026fac4626abbf90a67e0ed8061eb0
Sven Nilsen <bvssvni@gmail.com>
2014-05-02 09:58:16 +0200

    Reexport under top module

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,14 @@ extern crate graphics;
 extern crate opengles;
 extern crate glfw;
 
-pub mod game;
+pub use Game = game::Game;
+pub use GameWindow = game_window::GameWindow;
+pub use Settings = game::Settings;
+pub use Gl = gl::Gl;
+
 pub mod shader_utils;
-pub mod game_window;
-pub mod gl;
+
+mod game;
+mod game_window;
+mod gl;
 
