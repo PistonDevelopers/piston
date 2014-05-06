a2f99e07529de290cc3714ad8195dcfd0665e5a4
Sven Nilsen <bvssvni@gmail.com>
2014-05-06 19:57:07 +0200

    Added `AssetStore`

---
+++ src/asset_store.rs
@@ -0,0 +1,9 @@
+
+//! Storing sounds, textures, animations etc.
+
+/// A place to store sounds, textures, animations etc.
+///
+/// The idea is to have one object which the app can use  
+/// to load assets for the game with a simple interface.  
+pub struct AssetStore;
+
--- src/lib.rs
+++ src/lib.rs
@@ -17,6 +17,7 @@ pub use Game = game::Game;
 pub use GameWindow = game_window::GameWindow;
 pub use GameWindowSettings = game_window_settings::GameWindowSettings;
 pub use Gl = gl::Gl;
+pub use AssetStore = asset_store::AssetStore;
 
 pub mod shader_utils;
 
@@ -24,6 +25,7 @@ mod game;
 mod game_window;
 mod game_window_settings;
 mod gl;
+mod asset_store;
 
 // Temporary copied code from other projects.
 mod png;
