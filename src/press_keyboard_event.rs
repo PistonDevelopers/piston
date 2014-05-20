e7395b4f1cdae5fd629fcb775022439d857bb148
Coeuvre <coeuvre@gmail.com>
2014-05-20 20:29:39 +0800

    Add missing files

---
+++ src/press_keyboard_event.rs
@@ -0,0 +1,72 @@
+
+use graphics::Field;
+use piston::keyboard;
+use piston::event::{
+    Event,
+    KeyPressed,
+    KeyReleased,
+};
+
+use {
+    BackEnd,
+    Map,
+    Observer,
+};
+
+/// A event context which used to observe whether a certain keyboard key was pressed.
+pub struct PressKeyboardEvent<'a> {
+    /// The key to be observed.
+    pub key: Field<'a, keyboard::Key>,
+}
+
+impl<'a> Map<'a> for PressKeyboardEvent<'a> {
+    #[inline(always)]
+    fn map<'a, B: BackEnd>(&self, back_end: &mut B, command: ||: 'a) -> uint {
+        back_end.add_observer(box PressKeyboardEventObserver::new(command, *self.key.get()))
+    }
+}
+
+struct PressKeyboardEventObserver<'a> {
+    command: ||: 'a,
+    key: keyboard::Key,
+    can_trigger: bool,
+    is_pressed: bool,
+}
+
+impl<'a> PressKeyboardEventObserver<'a> {
+    pub fn new<'a>(command: ||: 'a, key: keyboard::Key) -> PressKeyboardEventObserver<'a> {
+        PressKeyboardEventObserver {
+            command: command,
+            key: key,
+            can_trigger: false,
+            is_pressed: false,
+        }
+    }
+}
+
+impl<'a> Observer for PressKeyboardEventObserver<'a> {
+    fn can_trigger(&self) -> bool {
+        self.can_trigger
+    }
+
+    fn trigger(&mut self) {
+        (self.command)();
+        self.can_trigger = false
+    }
+
+    fn on_event(&mut self, e: Event) {
+        match e {
+            KeyPressed(key) if key == self.key => {
+                if !self.is_pressed {
+                    self.is_pressed = true;
+                    self.can_trigger = true;
+                }
+            },
+            KeyReleased(key) if key == self.key => {
+                self.is_pressed = false;
+            }
+            _ => {}
+        }
+    }
+}
+
