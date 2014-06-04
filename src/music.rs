038cc0e3ed8378c0e9bae0ca164eb5f8b76692fa
Coeuvre <coeuvre@gmail.com>
2014-06-04 10:49:27 +0800

    Add a preliminary AudioBackEnd which is able to play sound

---
+++ src/music.rs
@@ -0,0 +1,13 @@
+
+#![allow(missing_doc)]
+
+/// Implemented by all the music.
+///
+/// Music is for long music like background music.
+pub trait Music {
+    /*
+    fn set_volumn(&mut self, volumn: f64);
+    fn get_volumn(&self) -> f64;
+    */
+}
+
---
+++ src/sound.rs
@@ -0,0 +1,13 @@
+
+#![allow(missing_doc)]
+
+/// Implemented by all the sound.
+///
+/// Sound is for short sound effects.
+pub trait Sound {
+    /*
+    fn set_volumn(&mut self, volumn: f64);
+    fn get_volumn(&self) -> f64;
+    */
+}
+
