fff4a303757c4ae78eb864c33a41726b2544773f
Sven Nilsen <bvssvni@gmail.com>
2014-06-21 11:21:58 +0200

    Changed  `render` on `Game` trait to take '&mut self'
    Closes https://github.com/PistonDevelopers/piston/issues/387

--- examples/image.rs
+++ examples/image.rs
@@ -32,7 +32,7 @@ impl Game for App {
         self.image = Some(Texture::from_path(&image).unwrap());
     }
 
-    fn render(&self, c: &Context, args: &mut RenderArgs) {
+    fn render(&mut self, c: &Context, args: &mut RenderArgs) {
         c.image(self.image.as_ref().unwrap()).draw(args.gl);
     }
 }
