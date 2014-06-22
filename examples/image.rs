323597fbbb4e3409679828df3c49a622b0cb4cd9
Sven Nilsen <bvssvni@gmail.com>
2014-06-22 13:33:01 +0200

    Added `GameIteratorSettings` argument to `Game::run`
    Closes https://github.com/PistonDevelopers/piston/issues/323

--- examples/image.rs
+++ examples/image.rs
@@ -7,6 +7,7 @@ use graphics::*;
 use piston::{
     AssetStore,
     Game,
+    GameIteratorSettings,
     GameWindowSDL2,
     GameWindowSettings,
     RenderArgs,
@@ -63,7 +64,11 @@ fn main() {
     let mut asset_store = AssetStore::from_folder("assets");
 
     let mut app = App::new();
-    app.run(&mut window, &mut asset_store);
+    let game_iter_settings = GameIteratorSettings {
+            updates_per_second: 120,
+            max_frames_per_second: 60,
+        };
+    app.run(&mut window, &mut asset_store, &game_iter_settings);
 }
 
 
