c02fe9891d9ee16245eaea8c53d062e9c5f6fd95
Greg M <morenzg@gmail.com>
2014-06-28 13:38:13 -0400

    fix pub use statements

--- src/lib.rs
+++ src/lib.rs
@@ -11,27 +11,6 @@ extern crate sync;
 pub use ConcurrentGame = concurrent_game::ConcurrentGame;
 pub use Game = game::Game;
 
-pub use Render = game_iterator::Render;
-pub use Update = game_iterator::Update;
-pub use KeyPress = game_iterator::KeyPress;
-pub use KeyRelease = game_iterator::KeyRelease;
-pub use MousePress = game_iterator::MousePress;
-pub use MouseRelease = game_iterator::MouseRelease;
-pub use MouseMove = game_iterator::MouseMove;
-pub use MouseRelativeMove = game_iterator::MouseRelativeMove;
-
-pub use GameEvent = game_iterator::GameEvent;
-pub use GameIterator = game_iterator::GameIterator;
-pub use GameIteratorSettings = game_iterator::GameIteratorSettings;
-pub use RenderArgs = game_iterator::RenderArgs;
-pub use UpdateArgs = game_iterator::UpdateArgs;
-pub use KeyPressArgs = game_iterator::KeyPressArgs;
-pub use KeyReleaseArgs = game_iterator::KeyReleaseArgs;
-pub use MousePressArgs = game_iterator::MousePressArgs;
-pub use MouseReleaseArgs = game_iterator::MouseReleaseArgs;
-pub use MouseMoveArgs = game_iterator::MouseMoveArgs;
-pub use MouseRelativeMoveArgs = game_iterator::MouseRelativeMoveArgs;
-
 pub use GameWindow = game_window::GameWindow;
 pub use RenderWindow = game_window::RenderWindow;
 pub use GameWindowSettings = game_window_settings::GameWindowSettings;
