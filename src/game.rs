4f21d55d652a275c1f4640320ad144bd0d8e57e5
Ben Foppa <benjamin.foppa@gmail.com>
2014-07-23 01:12:52 -0400

    pass game window as part of non-concurrent game events

--- src/game.rs
+++ src/game.rs
@@ -25,12 +25,12 @@ use Update;
 use UpdateArgs;
 
 /// Implemented by game applications.
-pub trait Game {
+pub trait Game<W: GameWindow> {
     /// Render graphics.
-    fn render(&mut self, _args: &RenderArgs) {}
+    fn render(&mut self, _window: &mut W, _args: &RenderArgs) {}
 
     /// Update the physical state of the game.
-    fn update(&mut self, _args: &UpdateArgs) {}
+    fn update(&mut self, _window: &mut W, _args: &UpdateArgs) {}
 
     /// Perform tasks for loading before showing anything.
     fn load(&mut self) {}
@@ -38,27 +38,27 @@ pub trait Game {
     /// User pressed a key.
     ///
     /// This can be overridden to handle key pressed events.
-    fn key_press(&mut self, _args: &KeyPressArgs) {}
+    fn key_press(&mut self, _window: &mut W, _args: &KeyPressArgs) {}
 
     /// User released a key.
     ///
     /// This can be overridden to handle key released events.
-    fn key_release(&mut self, _args: &KeyReleaseArgs) {}
+    fn key_release(&mut self, _window: &mut W, _args: &KeyReleaseArgs) {}
 
     /// Pressed a mouse button.
-    fn mouse_press(&mut self, _args: &MousePressArgs) {}
+    fn mouse_press(&mut self, _window: &mut W, _args: &MousePressArgs) {}
 
     /// Released a mouse button.
-    fn mouse_release(&mut self, _args: &MouseReleaseArgs) {}
+    fn mouse_release(&mut self, _window: &mut W, _args: &MouseReleaseArgs) {}
 
     /// Moved mouse cursor.
-    fn mouse_move(&mut self, _args: &MouseMoveArgs) {}
+    fn mouse_move(&mut self, _window: &mut W, _args: &MouseMoveArgs) {}
 
     /// Moved mouse relative, not bounded by cursor.
-    fn mouse_relative_move(&mut self, _args: &MouseRelativeMoveArgs) {}
+    fn mouse_relative_move(&mut self, _window: &mut W, _args: &MouseRelativeMoveArgs) {}
 
     /// Scrolled mouse.
-    fn mouse_scroll(&mut self, _args: &MouseScrollArgs) {}
+    fn mouse_scroll(&mut self, _window: &mut W, _args: &MouseScrollArgs) {}
 
     /// Whether the cursor should be captured.
     ///
@@ -69,22 +69,22 @@ pub trait Game {
     fn should_capture_cursor(&mut self) -> bool { false }
 
     /// Handles a game event.
-    fn event(&mut self, event: &mut GameEvent) {
+    fn event(&mut self, game_window: &mut W, event: &mut GameEvent) {
         match *event {
-            Render(ref mut args) => self.render(args),
-            Update(ref mut args) => self.update(args),
-            KeyPress(ref args) => self.key_press(args),
-            KeyRelease(ref args) => self.key_release(args),
-            MousePress(ref args) => self.mouse_press(args),
-            MouseRelease(ref args) => self.mouse_release(args),
-            MouseMove(ref args) => self.mouse_move(args),
-            MouseRelativeMove(ref args) => self.mouse_relative_move(args),
-            MouseScroll(ref args) => self.mouse_scroll(args),
+            Render(ref mut args) => self.render(game_window, args),
+            Update(ref mut args) => self.update(game_window, args),
+            KeyPress(ref args) => self.key_press(game_window, args),
+            KeyRelease(ref args) => self.key_release(game_window, args),
+            MousePress(ref args) => self.mouse_press(game_window, args),
+            MouseRelease(ref args) => self.mouse_release(game_window, args),
+            MouseMove(ref args) => self.mouse_move(game_window, args),
+            MouseRelativeMove(ref args) => self.mouse_relative_move(game_window, args),
+            MouseScroll(ref args) => self.mouse_scroll(game_window, args),
         }
     }
 
     /// Executes a game loop.
-    fn run<W: GameWindow>(
+    fn run(
         &mut self,
         game_window: &mut W,
         game_iter_settings: &GameIteratorSettings
@@ -101,7 +101,7 @@ pub trait Game {
         loop {
             match game_iter.next() {
                 None => break,
-                Some(mut e) => self.event(&mut e)
+                Some(mut e) => self.event(game_iter.game_window, &mut e)
             }
             if  self.should_capture_cursor() != should_capture_cursor {
                 should_capture_cursor = !should_capture_cursor;
