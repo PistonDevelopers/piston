12cdb5b76e6acae8f75fe187dc2395a87f930c97
Sven Nilsen <bvssvni@gmail.com>
2014-08-30 11:06:56 +0200

    Made game loop nicer
    Closes https://github.com/PistonDevelopers/piston/issues/606

--- src/game_iterator.rs
+++ src/game_iterator.rs
@@ -80,7 +80,7 @@ pub struct GameIteratorSettings {
 ///     }
 /// }
 /// ```
-pub struct GameIterator<'a, W> {
+pub struct GameIterator<'a, W: 'a> {
     /// The game window used by iterator.
     pub game_window: &'a mut W,
     state: GameIteratorState,
@@ -121,7 +121,7 @@ for GameIterator<'a, W> {
     /// Returns the next game event.
     fn next(&mut self) -> Option<GameEvent> {
         loop {
-            match self.state {
+            self.state = match self.state {
                 RenderState => {
                     if self.game_window.should_close() { return None; }
 
@@ -133,53 +133,47 @@ for GameIterator<'a, W> {
                         // Swap buffers next time.
                         self.state = SwapBuffersState;
                         return Some(Render(RenderArgs {
-                                // Extrapolate time forward to allow smooth motion.
-                                ext_dt: (start_render - self.last_update) as f64 / billion as f64,
-                                width: w,
-                                height: h,
-                            }
-                        ));
+                            // Extrapolate time forward to allow smooth motion.
+                            ext_dt: (start_render - self.last_update) as f64 / billion as f64,
+                            width: w,
+                            height: h,
+                        }));
                     }
 
-                    self.state = UpdateLoopState;
-                },
+                    UpdateLoopState
+                }
                 SwapBuffersState => {
                     self.game_window.swap_buffers();
-                    self.state = UpdateLoopState;
-                },
+                    UpdateLoopState
+                }
                 UpdateLoopState => {
                     let current_time = time::precise_time_ns();
                     let next_frame = self.last_frame + self.dt_frame_in_ns;
                     let next_update = self.last_update + self.dt_update_in_ns;
                     let next_event = cmp::min(next_frame, next_update);
                     if next_event > current_time {
-                        sleep( Duration::nanoseconds((next_event - current_time) as i32) );
+                        sleep( Duration::nanoseconds((next_event - current_time) as i64) );
+                        UpdateLoopState
                     } else if next_event == next_frame {
-                        self.state = RenderState;
+                        RenderState
                     } else {
-                        self.state = HandleEventsState;
+                        HandleEventsState
                     }
-                },
+                }
                 HandleEventsState => {
                     // Handle all events before updating.
-                    return match self.game_window.poll_event() {
-                        None => {
-                            self.state = UpdateState;
-                            // Explicitly continue because otherwise the result
-                            // of this match is immediately returned.
-                            continue;
-                        },
-                        Some(x) => Some(Input(x)),
+                    match self.game_window.poll_event() {
+                        None => UpdateState,
+                        Some(x) => { return Some(Input(x)); },
                     }
-                },
+                }
                 UpdateState => {
                     self.state = UpdateLoopState;
                     self.last_update += self.dt_update_in_ns;
-                    return Some(Update(UpdateArgs{
-                        dt: self.dt,
-                    }));
-                },
+                    return Some(Update(UpdateArgs{ dt: self.dt }));
+                }
             };
         }
     }
 }
+
