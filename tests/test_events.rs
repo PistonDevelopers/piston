e9a9cdd7c4da6ded34ee78b5e820c2709dfa067a
Jake Kerr <kodafox@gmail.com>
2014-10-08 01:11:35 +0900

    Move examples to tests as that's really what they are :)
    The tests could probably be organized a bit and named more descriptively but
    for now I'd just like to give us some coverage.

--- tests/test_events.rs
+++ tests/test_events.rs
@@ -1,10 +1,8 @@
-
-extern crate event;
-
 use event::{
     Action,
     State,
     Sequence,
+    Success,
     Update,
     UpdateArgs,
     Wait,
@@ -25,8 +23,8 @@ pub enum TestActions {
 fn exec(mut acc: u32, dt: f64, state: &mut State<TestActions, ()>) -> u32 {
     state.event(&Update(UpdateArgs { dt: dt }), |dt, action, _| {
         match *action {
-            Inc => { acc += 1; (event::Success, dt) },
-            Dec => { acc -= 1; (event::Success, dt) },
+            Inc => { acc += 1; (Success, dt) },
+            Dec => { acc -= 1; (Success, dt) },
         }
     });
     acc
@@ -36,6 +34,7 @@ fn exec(mut acc: u32, dt: f64, state: &mut State<TestActions, ()>) -> u32 {
 // consumes a time of 0.0 seconds.
 // This makes it possible to execute one action
 // after another without delay or waiting for next update.
+#[test]
 fn print_2() {
     let a: u32 = 0;
     let seq = Sequence(vec![Action(Inc), Action(Inc)]);
@@ -47,6 +46,7 @@ fn print_2() {
 // If you wait the exact amount before to execute an action,
 // it will execute. This behavior makes it easy to predict
 // when an action will run.
+#[test]
 fn wait_sec() {
     let a: u32 = 0;
     let seq = Sequence(vec![Wait(1.0), Action(Inc)]);
@@ -57,6 +57,7 @@ fn wait_sec() {
 
 // When we execute half the time and then the other half,
 // then the action should be executed.
+#[test]
 fn wait_half_sec() {
     let a: u32 = 0;
     let seq = Sequence(vec![Wait(1.0), Action(Inc)]);
@@ -67,7 +68,18 @@ fn wait_half_sec() {
     assert_eq!(a, 1);
 }
 
+// A sequence of one event is like a bare event.
+#[test]
+fn sequence_of_one_event() {
+    let a: u32 = 0;
+    let seq = Sequence(vec![Action(Inc)]);
+    let mut state = State::new(seq);
+    let a = exec(a, 1.0, &mut state);
+    assert_eq!(a, 1);
+}
+
 // A sequence of wait events is the same as one wait event.
+#[test]
 fn wait_two_waits() {
     let a: u32 = 0;
     let seq = Sequence(vec![Wait(0.5), Wait(0.5), Action(Inc)]);
@@ -77,6 +89,7 @@ fn wait_two_waits() {
 }
 
 // Increase counter ten times.
+#[test]
 fn loop_ten_times() {
     let a: u32 = 0;
     let rep = While(box Wait(50.0), vec![Wait(0.5), Action(Inc), Wait(0.5)]);
@@ -85,6 +98,7 @@ fn loop_ten_times() {
     assert_eq!(a, 10);
 }
 
+#[test]
 fn when_all_wait() {
     let a: u32 = 0;
     let all = Sequence(vec![
@@ -99,11 +113,3 @@ fn when_all_wait() {
     assert_eq!(a, 1);
 }
 
-fn main() {
-    print_2();
-    wait_sec();
-    wait_half_sec();
-    wait_two_waits();
-    loop_ten_times();
-    when_all_wait();
-}
