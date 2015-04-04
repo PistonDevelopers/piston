333d5567f99007801a2f757f1b1fc0f6897545ef
Sven Nilsen <bvssvni@gmail.com>
2015-04-04 02:15:44 +0200

    Updated to latest Rust
    Closes https://github.com/PistonDevelopers/piston/issues/867

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,6 @@
 #![crate_name = "piston"]
 #![deny(missing_docs)]
 #![warn(dead_code)]
-#![feature(macro_reexport)]
 
 //! A user friendly game engine written in Rust.
 
@@ -9,22 +8,9 @@
 extern crate input as input_lib;
 extern crate event as event_lib;
 extern crate window as window_lib;
-#[macro_reexport(quack, quack_get, quack_set, quack_action, quack_macro_items)]
-extern crate quack as quack_lib;
 
 pub use input_lib as input;
 pub use event_lib as event;
 pub use window_lib as window;
-pub use quack_lib as quack;
 
 pub use event::events;
-
-pub use quack::{
-    Action,
-    ActOn,
-    Get,
-    GetFrom,
-    Set,
-    SetAt,
-};
-
